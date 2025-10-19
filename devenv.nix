0537dea32d2d0bc80e03d45599fd7879c536ddce
Pabocalypse <144961459+pabocalypse@users.noreply.github.com>
2025-10-19 11:56:26 +0100

    chore: Add pre-commit hooks via devenv

--- devenv.nix
+++ devenv.nix
@@ -2,4 +2,10 @@
 
 {
   packages = [ pkgs.git pkgs.ruby pkgs.bundler pkgs.just pkgs.tzdata ];
+
+  git-hooks.hooks = {
+    nixpkgs-fmt.enable = true;
+    check-merge-conflicts.enable = true;
+  };
+
 }
