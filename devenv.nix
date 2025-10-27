619522b8195f594e3c6c3caeb9fb3088ff883584
Pabocalypse <144961459+pabocalypse@users.noreply.github.com>
2025-10-27 09:41:06 +0100

    chore: add scripts to devenv
    This adds the `just` aliases into devenv as scripts to make them simpler
    to run when in a devenv shell. They call just directly rather than being
    a copy and paste of the same command so that it is only updated in a
    single place.

--- devenv.nix
+++ devenv.nix
@@ -1,11 +1,29 @@
 { pkgs, ... }:
 
 {
-  packages = [ pkgs.git pkgs.ruby pkgs.bundler pkgs.just pkgs.tzdata ];
+  packages = [
+    pkgs.bundler # Package installer for ruby
+    pkgs.git # Make sure we have git for committing
+    pkgs.just # Helper tool for creating script snippets
+    pkgs.ruby # Main language for jekyll
+    pkgs.tzdata # Used to set timezone data
+  ];
 
   git-hooks.hooks = {
     nixpkgs-fmt.enable = true;
     check-merge-conflicts.enable = true;
   };
+
+  scripts.install.exec = ''
+    ${pkgs.just}/bin/just install
+  '';
+
+  scripts.serve.exec = ''
+    ${pkgs.just}/bin/just serve
+  '';
+
+  scripts.clean.exec = ''
+    ${pkgs.just}/bin/just clean
+  '';
 
 }
